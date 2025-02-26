262f2d26d3eb6d511d9b2af363612442865c51a7
YoungJun Cho <yj44.cho@gmail.com>
2025-02-26 23:02:06 +0900

    Use calculate_rate_of_returns() in adm for performance
    Signed-off-by: YoungJun Cho <yj44.cho@gmail.com>

--- adm.py
+++ adm.py
@@ -4,28 +4,20 @@ class ADM:
     def __init__(self, common, daa):
         self._common = common
         self._daa = daa
+        self._assets = [
+            "SPY",  # SPDR S&P 500 | US Stocks
+            "IEFA",  # iShares Core MSCI EAFE : Developed Market Stocks
+            "BIL",  # SPDR Bloomberg Barclay 1-3 Month T-Bill : Cash
+        ]
 
     def calculate(self):
-        spy_ror = self._calculate_rate_of_return("SPY")    # SPDR S&P 500 | US Stocks
-        if spy_ror is None:
-            print("Warning: No data for SPY. Stopped.")
-            return []
-        efa_ror = self._calculate_rate_of_return("IEFA")  # iShares Core MSCI EAFE : Developed Market Stocks
-        if efa_ror is None:
-            print("Warning: No data for IEFA. Stopped.")
-            return []
-        bil_ror = self._calculate_rate_of_return("BIL")    # SPDR Bloomberg Barclay 1-3 Month T-Bill : Cash
-        if bil_ror is None:
-            print("Warning: No data for BIL. Stopped.")
+        rors = self._common.calculate_rate_of_returns(self._assets, 12)
+        if any(rors[ticker] is None for ticker in self._assets):
+            print("Warning: Missing data for one or more tickers. Stopped.")
             return []
 
-        if spy_ror > bil_ror:
-            if spy_ror >= efa_ror:
-                return ["SPY"]
-            else:
-                return ["IEFA"]
-        else:
-            return self._daa.calculate()
+        spy_ror = rors["SPY"]
+        iefa_ror = rors["IEFA"]
+        bil_ror = rors["BIL"]
 
-    def _calculate_rate_of_return(self, ticker):
-        return self._common.calculate_rate_of_return(ticker, 12)
+        return [max("SPY", "IEFA", key=lambda x: rors[x])] if spy_ror > bil_ror else self._daa.calculate()
